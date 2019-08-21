461913ec5b8fcecb97dcd9db12989a6761fe63f9
Milena Kari <milenakari@yahoo.com>
2019-08-21 01:05:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+class Driver{
+  constructor(name, dateString){
+    this.name=name;
+    this.startDate = new Date(dateString)
+  }
+  
+  yearsExperienceFromBeginningOf(startYear){
+    return startYear - 1900 - this.startDate.getYear()
+  }
+}
